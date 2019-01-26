3353f60a3b5ef21f0201f7b660d2af5c00e58b45
Giacomo Ravaioli <giackarancio@hotmail.it>
2019-01-26 15:33:24 +0100

    add first level structure

---
+++ Assets/CameraFollowBehav.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowBehav : MonoBehaviour
+{
+
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag(Tags.Player);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+    }
+}
