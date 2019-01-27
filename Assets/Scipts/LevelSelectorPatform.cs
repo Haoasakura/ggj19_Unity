6581caf67eeff68ac4179b3c0a266cf30a286c21
Unknown <kingofbandit27@gmail.com>
2019-01-27 11:07:01 +0100

    Burrito

--- Assets/Scipts/LevelSelectorPatform.cs
+++ Assets/Scipts/LevelSelectorPatform.cs
@@ -7,6 +7,10 @@ public class LevelSelectorPatform : MonoBehaviour
 {
 	public new string name="";
 
+	private void Start() {
+		//deactivate evenn when shece reloaded
+	}
+
 	private void OnTriggerEnter2D(Collider2D collision) {
 		if(collision.CompareTag(Tags.Player))
 			SceneManager.LoadScene(name);
