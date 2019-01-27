202c2842dfbd4a7b695bccf6da1369ece8d1ccf1
Unknown <kingofbandit27@gmail.com>
2019-01-27 14:15:29 +0100

    wall sprite, added wall to level1 remover particle pack

---
+++ Assets/Scipts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+	public float m_speed;
+
+	private Vector3 m_speedVector;
+
+	private void Start() {
+		m_speedVector = new Vector3(0f, m_speed, 0f);
+	}
+	void Update()
+    {
+		transform.Rotate(Vector3.up,m_speed*Time.deltaTime);
+    }
+}
