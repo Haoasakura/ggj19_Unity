3d250688ce5ff546e35fe8593d84dd04e0d558df
Unknown <kingofbandit27@gmail.com>
2019-01-28 17:37:59 +0100

    multiple food sprites

--- Assets/Scipts/WallThrow.cs
+++ Assets/Scipts/WallThrow.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class WallThrow : MonoBehaviour
 {
 	public float m_throwDelay=1.0f;
+	public Sprite[] m_sprites=new Sprite[2];
 
 	public GameObject m_projectile;
-	BoxCollider2D m_boxCollider2D;
+
+	private bool m_lastSprite;
+
+	private BoxCollider2D m_boxCollider2D;
 
 	private void Awake() {
 		m_boxCollider2D = GetComponent<BoxCollider2D>();
@@ -24,7 +28,8 @@ public class WallThrow : MonoBehaviour
 		while(true) {
 
 			GameObject projectile= Instantiate(m_projectile, m_boxCollider2D.bounds.center,Quaternion.identity);
-
+			projectile.GetComponent<SpriteRenderer>().sprite = m_lastSprite ? m_sprites[0] : m_sprites[1];
+			m_lastSprite = !m_lastSprite;
 			yield return waitTime;
 		}
 	}
